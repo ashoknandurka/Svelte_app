6a44dbab2837f89eb8c6013a11762d825ec1404b
ashoknandurka <ashok.nandurka@predera.com>
2024-02-05 19:58:36 +0530

    stores functionality added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
+<script>
+// @ts-nocheck
+
+	import { page, navigating } from '$app/stores';
+</script>
+
 <nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
+	<a href="/" aria-current={$page.url.pathname === '/'}>home</a>
+	<a href="/about" aria-current={$page.url.pathname === '/about'}>about</a>
     <a href="/blog">blog</a>
 	<a href="/todoAction">todo</a>
 	<a href="/todos">todos</a>
     <a href="/a/deeply/nested/route">A deeply nested route</a>
+
+	{#if $navigating}
+	navigating to {$navigating.to.url.pathname}
+   {/if}
+
 </nav>
 
 <slot />
